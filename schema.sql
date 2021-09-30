8ef1855bc1a0acbfd29b403898e7185cecb2ee6b
Alena <alenalswann@gmail.com>
2021-09-30 15:56:52 -0700

    Added retiring employee counts by department:

--- schema.sql
+++ schema.sql
@@ -55,3 +55,42 @@ CREATE TABLE titles (
 );
 
 SELECT * FROM departments;
+
+-- Create new table of queried info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees with emp_no
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Add current retiring employees join into new table
+Select * FROM retirement_info;
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01')
+
+-- Create new table of retiring employee counts by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retiring_dept_counts
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
